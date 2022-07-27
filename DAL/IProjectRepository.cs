23e9c6abd7dfa57c311b0e1426bfefc1df8d7039
Reza Kalhor <reza.kalhor19@gmail.com>
2022-07-27 11:07:37 +0430

    Project Repository modified

--- DAL/IProjectRepository.cs
+++ DAL/IProjectRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class IProjectRepository
+    public interface IProjectRepository : IRepository<Project>
     {
     }
 }
--- DAL/ISavedProjectRepository.cs
+++ DAL/ISavedProjectRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class ISavedProjectRepository
+    public interface ISavedProjectRepository : IRepository<SavedProject>
     {
     }
 }
