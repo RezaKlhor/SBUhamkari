09d550638ef9aff8524f6f5064d49345235c24ad
Reza Kalhor <reza.kalhor19@gmail.com>
2022-08-09 02:53:47 +0430

    TA application repo completed

--- DAL/UserRepos/ITAapplicationRepository.cs
+++ DAL/UserRepos/ITAapplicationRepository.cs
@@ -8,5 +8,7 @@ namespace DAL.UserRepos
 {
     public interface ITAapplicationRepository : IRepository<TAapplication>
     {
+        List<TAapplication> GetTAapplicationsByTArequest(Guid tareqID);
+        List<TAapplication> GetAapplicationsByStudentWithTArequest(Guid studentID);
     }
 }
--- DAL/UserRepos/TAapplicationRepository.cs
+++ DAL/UserRepos/TAapplicationRepository.cs
@@ -14,5 +14,15 @@ namespace DAL.UserRepos
         {
         }
         public HamkariContext HamkariContext { get { return Context as HamkariContext; } }
+
+        public List<TAapplication> GetAapplicationsByStudentWithTArequest(Guid studentID)
+        {
+            return HamkariContext.TAapplications.Where(m => m.Student.id == studentID).Include(m => m.Tarequest).ToList();
+        }
+
+        public List<TAapplication> GetTAapplicationsByTArequest(Guid tareqID)
+        {
+            return Find(m => m.Tarequest.id == tareqID).ToList();
+        }
     }
 }
