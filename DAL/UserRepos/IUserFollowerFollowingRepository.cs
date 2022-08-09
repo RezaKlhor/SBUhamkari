5a423aefb8b8ceb05375ce19e98f953c55b113ab
Reza Kalhor <reza.kalhor19@gmail.com>
2022-08-09 10:30:48 +0430

    Following repo completed

--- DAL/UserRepos/IUserFollowerFollowingRepository.cs
+++ DAL/UserRepos/IUserFollowerFollowingRepository.cs
@@ -3,5 +3,6 @@
     public interface IUserFollowerFollowingRepository : IRepository<Following>
     {
         List<Following> GetFollowingsByFollowerID(Guid id);
+        List<Following> GetFollowingsByFollowedID(Guid id);
     }
 }
--- DAL/UserRepos/UserFollowerFollowingRepository.cs
+++ DAL/UserRepos/UserFollowerFollowingRepository.cs
@@ -13,6 +13,11 @@ namespace DAL.UserRepos
 
         public HamkariContext HamkariContext { get { return Context as HamkariContext; } }
 
+        public List<Following> GetFollowingsByFollowedID(Guid id)
+        {
+            return Find(m => m.FollowedID == id).ToList();
+        }
+
         public List<Following> GetFollowingsByFollowerID(Guid id)
         {
             return Find(m => m.Follower.id == id).ToList();
