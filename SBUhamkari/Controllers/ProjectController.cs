c12a2b5c72f2fe07fe6938d8ec69f7c2203797aa
Reza Kalhor <reza.kalhor19@gmail.com>
2022-08-10 23:18:44 +0430

    Controllers initialized

---
+++ SBUhamkari/Controllers/ProjectController.cs
@@ -0,0 +1,49 @@
+﻿using DAL;
+
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.Models;
+
+namespace SBUhamkari.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ProjectController> _logger;
+        private readonly HamkariContext database;
+
+        public ProjectController(ILogger<ProjectController> logger, HamkariContext databaseContext)
+        {
+            database = databaseContext;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Project> GetAllProjects()
+        {
+            using(UnitOfWork unitOfWork = new UnitOfWork(database))
+            {
+                var projects= unitOfWork.Projects.GetAll();
+                if (projects.Count() != 0)
+                {
+                    return (List<Project>)projects;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
--- SBUhamkari/Program.cs
+++ SBUhamkari/Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+ 
 app.UseAuthorization();
 
 app.MapControllers();
